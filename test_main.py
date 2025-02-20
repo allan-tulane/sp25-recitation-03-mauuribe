98f006fc6e3668731268671b6f144ce87d565a7f
mauuribe <97479708+mauuribe@users.noreply.github.com>
2025-02-20 16:38:46 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,3 +5,8 @@ from main import *
 ## Feel free to add your own tests here.
 def test_multiply():
     assert quadratic_multiply(BinaryNumber(3), BinaryNumber(3)) == 3*3
+    assert quadratic_multiply(BinaryNumber(4), BinaryNumber(4)) == 4*4
+    assert quadratic_multiply(BinaryNumber(6), BinaryNumber(5)) == 6*5
+    assert quadratic_multiply(BinaryNumber(14), BinaryNumber(10)) == 14*10
+    assert quadratic_multiply(BinaryNumber(27), BinaryNumber(5)) == 27*5
+    assert quadratic_multiply(BinaryNumber(11), BinaryNumber(3)) == 11*3
